db23488b4691330414b064c08b07b63c7622500f
jspencerhopkins <jspencerhopkins@gmail.com>
2015-02-09 19:56:03 -0500

    Differences all set

--- app/controllers/differences_controller.rb
+++ app/controllers/differences_controller.rb
@@ -1,2 +1,41 @@
 class DifferencesController < ApplicationController
+
+	def index
+		@differences = Difference.all
+	end
+
+	def new
+		@difference = Difference.new
+	end
+
+	def create
+	  @difference = Difference.create(difference_params)
+	  redirect_to differences_path
+	end
+
+	def show
+		 @difference = Difference.find(params[:id])
+	end
+
+	def edit
+  	@difference = Difference.find(params[:id])
+	end
+
+	def update
+		@difference = Difference.find(params[:id])
+		@difference.update(difference_params)
+		redirect_to difference_path
+	end
+
+	def destroy
+    @difference = Difference.find(params[:id])
+    @difference.destroy
+    redirect_to differences_path
+  end
+
+	private
+	  def difference_params
+	    params.require(:difference).permit(:title, :text)
+	  end
+
 end
